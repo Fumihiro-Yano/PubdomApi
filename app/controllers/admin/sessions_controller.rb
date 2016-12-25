bafeedb6affe1a62c1fd8261d53b8dcb972cb0b9
矢野史洋 <a2.mj.pop@gmail.com>
2016-12-25 17:05:01 +0900

    認証ができなかった場合は404のステータスを返すように変更

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -4,11 +4,15 @@ class Admin::SessionsController < Admin::Base
   def login_user_info
   	@user = User.find_by(email: params[:email].downcase)
   	if ! @user
-      render json: false
+      render json: false, status:404
   	  return
   	end
   	@authenticate = @user.authenticate(params[:password])
   	#authenticateは認証に成功したら、user情報を返す。失敗したらfalseを返す
+    if ! @authenticate
+      render json: @authenticate, status:404
+      return
+    end
   	render json: @authenticate
   end
 end
